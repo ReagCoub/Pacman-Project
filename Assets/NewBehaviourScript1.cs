417dfbbb1be448af71120adef79b7afface2d265
ReagCoub <reagan.courbron@baxter-academy.org>
2016-09-22 11:01:16 -0400

    First commit

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour
+
+{
+	public Rigidbody rb;
+	public float speed = 0.3f;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Vector3 position = this.transform.position;
+
+			rb.velocity = Vector3.left;
+			transform.position = position;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 position = this.transform.position;
+
+			rb.velocity = Vector3.right;
+			transform.position = position;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			Vector3 position = this.transform.position;
+
+			rb.velocity = Vector3.up;
+			transform.position = position;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			Vector3 position = this.transform.position;
+
+			rb.velocity = Vector3.down;
+			transform.position = position;
+		} else {
+			 
+			//set velocity to zer0
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.contacts [0].point.y < transform.position.y){
+			print ("aaahh");
+		}
+	}
+}
